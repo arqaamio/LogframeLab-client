6a405bd7694a420571a7b32268d8916e008c7047
VTXRyder1300C <maporshe@aol.com>
2020-05-03 16:57:14 +0900

    <#4mm3rz> Create_pop-up_for_website

---
+++ src/app/dialog/dialog.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input, Output, OnChanges, EventEmitter } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+@Component({
+  selector: 'app-pop',
+  templateUrl: './dialog.component.html',
+  styleUrls: ['./dialog.component.css'],
+  animations: [
+    trigger('dialog', [
+      transition('void => *', [
+        style({ transform: 'scale3d(.3, .3, .3)' }),
+        animate(100)
+      ]),
+      transition('* => void', [
+        animate(100, style({ transform: 'scale3d(.0, .0, .0)' }))
+      ])
+    ])
+  ]
+})
+export class DialogComponent implements OnInit {
+  @Input() closable = true;
+  @Input() visible: boolean;
+  @Output() visibleChange: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  ngOnInit() {
+    var firstTime = sessionStorage.getItem("firstTime");//Line 26-31, Pop-up only once per session
+    if(! firstTime){ 
+      this.visible = true;
+      sessionStorage.setItem("firstTime", "true");//hide line 29 for pop-up maintenance
+    }else{
+      this.visible = false;
+    }
+   }
+  close() {
+    this.visible = false;
+    this.visibleChange.emit(this.visible);
+  }
+}
