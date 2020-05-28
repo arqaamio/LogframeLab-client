fdaf9f2e161b3e68619e0a6f0ef102c99555423c
Ari <61361948+git-ari@users.noreply.github.com>
2020-05-28 11:31:04 +0100

    Update environment.prod.ts
    The previous PR didn't fully work because the HTTPS certificate isn't for the elasticbeanstalk URL but for the logframelab. So I added a record in Route 53 so that api.logframelab.ai is the elasticbeanstalk url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://logframelabserver-env.eba-rea7cmic.us-east-1.elasticbeanstalk.com'
+  apiBaseUrl: 'https://api.logframelab.ai'
 };
