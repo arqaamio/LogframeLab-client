ca6a9be43076105f8480e3de46e2a6a13277a7a0
Ari <61361948+git-ari@users.noreply.github.com>
2020-06-19 14:29:31 +0100

    Fix revert commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
-  apiBaseUrl: 'http://localhost:8082'
-=======
   apiBaseUrl: 'https://logframelab.ai'
->>>>>>> parent of c766b3d... Fix apiBaseUrl
 };
