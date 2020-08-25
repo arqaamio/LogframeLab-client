50371545305de1639cd473ec3a0d5e6f26b89d0a
Ari <61361948+git-ari@users.noreply.github.com>
2020-08-25 17:54:32 +0100

    Renamed error page to not found and added image

--- src/app/pages/notfound/notfound.component.ts
+++ src/app/pages/notfound/notfound.component.ts
@@ -2,11 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
-  selector: 'error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  selector: 'not-found',
+  templateUrl: './notfound.component.html',
+  styleUrls: ['./notfound.component.scss']
 })
-export class ErrorComponent implements OnInit {
+export class NotFoundComponent implements OnInit {
 
   constructor(private router: Router) { }
 
