fa092c4fd1538e6f61b76b97ed4cc6f6bfb3e132
Ari <61361948+git-ari@users.noreply.github.com>
2020-10-21 16:52:03 +0100

    Add edit popup and removed filters button on approval-uploaded

--- src/app/manage-indicators/manage-indicators.component.ts
+++ src/app/manage-indicators/manage-indicators.component.ts
@@ -12,10 +12,6 @@ import {IndicatorsManagement} from './utils/indicators-management';
 })
 export class ManageIndicatorsComponent extends IndicatorsManagement implements OnInit {
 
-  displayCrudModal = false;
-  operation: Operation;
-  indicator: IndicatorDto;
-
   constructor(public manageIndicatorsService: ManageIndicatorsService, public indicatorService: IndicatorService) {
     super(manageIndicatorsService, indicatorService);
   }
@@ -29,37 +25,6 @@ export class ManageIndicatorsComponent extends IndicatorsManagement implements O
     this.displayCrudModal = true;
   }
 
-  hideCrudModal(event) {
-    this.displayCrudModal = !event;
-    this.operation = undefined;
-    this.indicator = undefined;
-  }
-
-  /**
-   * Refresh indicator list with new API request
-   * @param refresh Wether to refresh to page 1
-   */
-  refreshIndicatorList(refresh: boolean) {
-    this.search(refresh, false);
-  }
-
-  edit(ind: IndicatorDto) {
-    this.displayIndicatorInModal(Operation.UPDATE, ind);
-  }
-
-  delete(ind: IndicatorDto) {
-    this.displayIndicatorInModal(Operation.DELETE, ind);
-  }
-
-  read(ind: IndicatorDto) {
-    this.displayIndicatorInModal(Operation.READ, ind);
-  }
-
-  private displayIndicatorInModal(operation: Operation, ind: IndicatorDto) {
-    this.operation = operation;
-    this.indicator = ind;
-    this.displayCrudModal = true;
-  }
 }
 
 
